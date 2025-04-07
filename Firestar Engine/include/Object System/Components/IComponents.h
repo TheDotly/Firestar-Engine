e96bb6a8b9cdcfa792e1bf5db524a320b1527b45
Obi-Wan Codenobi <Joshua.Gessner5@gmail.com>
2025-04-07 14:50:46 +1000

    Started Object System. :-)

---
+++ Firestar Engine/include/Object System/Components/IComponents.h
@@ -0,0 +1,11 @@
+#pragma once
+ 
+
+class IComponents
+{
+private:
+    /* data */
+public:
+    IComponents(/* args */);
+    ~IComponents();
+};
--- Firestar Engine/include/Object System/Object.h
+++ Firestar Engine/include/Object System/Object.h
@@ -1,2 +1,27 @@
 #pragma once
 
+#include <string>
+#include <vector>
+
+#include "Object System/Components/IComponents.h"
+
+class Object
+{
+private:
+    
+public:
+    std::string name = "";
+    bool isActive = true;
+    std::vector<IComponents> components;
+
+    // Functions 
+    // - Save to File (including Component states to create prefabs)
+    // - Static load from File returns object
+    
+
+    Object(std::string name, bool isActive = true, std::vector<IComponents> components);
+    Object(std::string name, bool isActive = true);
+
+    
+};
+
